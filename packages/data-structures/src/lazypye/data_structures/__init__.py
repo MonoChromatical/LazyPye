7006993f97fad782ffa005e4f80ab2f61fe22bf8
MechtrixPrime <126152660+MechtrixPrime@users.noreply.github.com>
2026-09-04 14:09:53 +0800

    added DoublyLinkedList to the "__init__.py"

--- packages/data-structures/src/lazypye/data_structures/__init__.py
+++ packages/data-structures/src/lazypye/data_structures/__init__.py
@@ -1,3 +1,4 @@
 from .singly_linked_list import SinglyLinkedList
+from .doubly_linked_list import DoublyLinkedList
 
-__all__ = ["SinglyLinkedList"]
+__all__ = ["SinglyLinkedList", "DoublyLinkedList"]
